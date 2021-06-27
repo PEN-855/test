eb280d02be657a99a5d2da2387f229508efccb28
PEN-855 <84565661+PEN-855@users.noreply.github.com>
2021-06-27 18:29:01 +0100

    Update main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     FlamingFighter.setImage(assets.image`Flaming Fighter - facing backwards`)
 })
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    projectile2 = sprites.createProjectileFromSprite(assets.image`lazer weapon`, FlamingFighter, 30, 30)
+})
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     projectile = sprites.createProjectileFromSprite(assets.image`Weapon`, FlamingFighter, 30, 30)
 })
@@ -9,6 +12,8 @@ controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
 })
 scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.doorClosedNorth, function (sprite, location) {
     tiles.setTilemap(tilemap`Level 2`)
+    DemonSnake.setPosition(randint(30, 160), randint(30, 160))
+    FlamingFighter.setPosition(144, 97)
 })
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     FlamingFighter.setImage(assets.image`Flaming Fighter - facing right`)
@@ -17,11 +22,13 @@ controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
     FlamingFighter.setImage(assets.image`Flaming Fighter - facing forwards`)
 })
 let projectile: Sprite = null
+let projectile2: Sprite = null
+let DemonSnake: Sprite = null
 let FlamingFighter: Sprite = null
 info.setScore(0)
 info.setLife(3)
 FlamingFighter = sprites.create(assets.image`Flaming Fighter`, SpriteKind.Player)
-let DemonSnake = sprites.create(assets.image`Demon Snake`, SpriteKind.Enemy)
+DemonSnake = sprites.create(assets.image`Demon Snake`, SpriteKind.Enemy)
 DemonSnake.setPosition(randint(30, 160), randint(30, 160))
 FlamingFighter.setPosition(10, 180)
 tiles.setTilemap(tilemap`Level 1`)
